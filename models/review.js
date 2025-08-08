0fcec6acedc67dca3126968ad79c0ab3055a33c1
Katie Hill <khill.fr.gr@gmail.com>
2025-08-08 16:01:33 +0100

    Renamed ratings review

---
+++ models/review.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  target_user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5 // ajusta según tu escala
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Review = mongoose.model('Review', reviewSchema)
+
+export default Review
