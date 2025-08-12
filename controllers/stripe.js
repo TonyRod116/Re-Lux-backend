11698fb5a9a10eb952197fbdf340b973a367efb5
Katie Hill <khill.fr.gr@gmail.com>
2025-08-12 11:54:29 +0100

    Stripe route and Secret Key added

---
+++ controllers/stripe.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import Stripe from 'stripe'
+
+const router = express.Router()
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+// Stripe route
+router.post('/purchase-intent', async (req, res, next) => {
+    try {
+        const { amount } = req.body
+        const payment = await stripe.payments.create({
+            amount,
+            currency: 'eur',
+            automatic_payment_methods: { enabled: true },
+        })
+        res.json({ clientSecret: payment.client_secret })
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
+
+
+export { router as stripeRouter }
